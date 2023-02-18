1965c37479184d2d1e561a623cab1d4d34264c9d
zerubabel095 <zerubabel095@gmail.com>
2023-02-18 14:57:33 -0800

    Puts function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -8,8 +8,7 @@
 
 int main(void)
 {
-	char Mystr[] = "\"Programming is like building a multilingual puzzle";
-	
+	char Mystr[] = "\"Programming is like building a multilingual puzzle";	
 	puts(Mystr);
 	return (0);
 }
