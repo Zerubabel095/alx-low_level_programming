9b51b7b8427a35ee43ef33e313e36a15848f0cb2
zerubabel095 <zerubabel095@gmail.com>
2023-02-20 16:59:50 -0800

    My greatest track

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,15 +1,17 @@
 #include <stdio.h>
 #include <string.h>
-/*
- * main - Prints lowercase alphabets
- * Return: Always 0 (success)
-*/
+/**
+ * main - prints lower case alphabets
+ *
+ * Return: Always 0 (Success)
+ */
+
 int main(void)
 {
 	char ch = 'a';
 	for (ch = 'a'; ch <= 'z'; ch++)
 	{
-        	putchar(ch);
+		putchar(ch);
 	}
 	putchar('\n');
 	return (0);
