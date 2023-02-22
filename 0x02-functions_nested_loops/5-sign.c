1c161310c8f5832befd1c9a0c228970b75393954
zerubabel095 <zerubabel095@gmail.com>
2023-02-22 14:01:51 -0800

    My best one

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -10,17 +10,19 @@
   */
 int print_sign(int n)
 {
-	if (n > 0 && n == 0 && n < 0)
+	if (n > 0)
 	{
-	 	_putchar('+');
+	 	_putchar(43);
 	 	return (1);
 	}
+	else if (n < 0)
 	{
-		_putchar('0');
+		_putchar(48);
 		return (0);
 	}
+	else
 	{
-		_putchar('-');
+		_putchar(45);
 		return (-1);
 	}
 }
