a504bfd5291a06db5d91df383b56cf3e3ddb1b06
zerubabel095 <zerubabel095@gmail.com>
2023-02-22 14:14:39 -0800

    My first commit in sign of number

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -3,8 +3,7 @@
 
 /**
   * print_sign - Checks for sign of a number
-  *
-  * @c: The character to be checked
+  * @n: The number to be checked
   *
   * Return: 1 for n greater than 0 or 0 for n equal to 0 and -1 for n lessthan 0
   */
@@ -12,17 +11,17 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
-	 	_putchar(43);
-	 	return (1);
+		_putchar(43);
+		return (1);
 	}
 	else if (n < 0)
 	{
-		_putchar(48);
-		return (0);
+		_putchar(45);
+		return (-1);
 	}
 	else
 	{
-		_putchar(45);
-		return (-1);
+		_putchar(48);
+		return (0);
 	}
 }
