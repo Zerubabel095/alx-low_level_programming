408aa38338efc200174d29d265c765d8e3ceb64c
zerubabel095 <zerubabel095@gmail.com>
2023-02-28 14:53:14 -0800

    pointers are different

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *  reser_to_98 - Takes pointer and updates
+ *  @n: An integer input
+ *  Return : nothing or n
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
