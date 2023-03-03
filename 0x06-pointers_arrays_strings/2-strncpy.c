5d3bd2cb6855c8a79d0e54c624c1773ac4669aa9
zerubabel095 <zerubabel095@gmail.com>
2023-03-03 12:10:16 -0800

    strncpy

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * _strncpy - a function that copies a string
+ * @src - source file
+ * @dest - destination folder
+ * Return: dest
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+	for (i = 0; i < n && src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	
+	}
+	for ( ; i < n; i++)
+	{
+		dest[i] = '\0';
+	}
+	return (dest);
+}
